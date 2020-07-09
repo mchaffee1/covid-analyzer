a6db423968dadbd57335444d28e58379aa1f3126
Michael Chaffee <mchaffee@thoughtworks.com>
2020-07-09 04:28:04 -1000

    update NYT data; add super-simple copy script

---
+++ copysimple.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp ../covid-19-data/us-states.csv ./Sources/App/Resources/
+cp ../covid-19-data/us-counties.csv ./Sources/App/Resources/
+cp ../covid-19-data/us.csv ./Sources/App/Resources/
+
